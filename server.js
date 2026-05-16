d5549eb23bfbf3069d94c274a2ab48b0af9deeb2
cmttiktok <phamanhtu535@gmail.com>
2026-05-17 01:18:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 });
 
 // Cấu hình Port cho Render
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
     console.log(`Server đang chạy trên port: ${PORT}`);
 });
